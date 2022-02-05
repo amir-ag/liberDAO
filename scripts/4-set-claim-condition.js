3528f9fded328b07a9b41da196ed45acb55b1e46
Amir <40580705+amir-ag@users.noreply.github.com>
2022-02-05 22:18:25 +0100

    add minting logic and claim conditions

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,22 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x0a293C251B34734D574F09B06f68Ee19F49Dd240",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 21,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,97 @@
-import { useEffect, useMemo, useState } from "react";
-import { useWeb3 } from "@3rdweb/hooks";
+import { useEffect, useMemo, useState } from 'react';
+import { useWeb3 } from '@3rdweb/hooks';
+import { ThirdwebSDK } from '@3rdweb/sdk';
+
+const sdk = new ThirdwebSDK('rinkeby');
+
+const bundleDropModule = sdk.getBundleDropModule(
+  '0x0a293C251B34734D574F09B06f68Ee19F49Dd240'
+);
 
 const App = () => {
   const { connectWallet, address, error, provider } = useWeb3();
-  console.log("👋 Address:", address)
+
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  const signer = provider ? provider.getSigner() : undefined;
+
+  useEffect(() => {
+    sdk.setProviderOrSigner(signer);
+  }, [signer]);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+
+    return bundleDropModule
+      .balanceOf(address, '0')
+      .then((balance) => {
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log('🌟 this user has a membership NFT!');
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      })
+      .catch((error) => {
+        setHasClaimedNFT(false);
+        console.error('failed to nft balance', error);
+      });
+  }, [address]);
 
   //the user hasn't connected their wallet yet
   if (!address) {
     return (
       <div className="landing">
         <h1>Welcome to LiberDAO</h1>
-        <button onClick={() => connectWallet("injected")} className="btn-hero">
+        <button onClick={() => connectWallet('injected')} className="btn-hero">
           Connect your wallet
         </button>
       </div>
     );
   }
-  
-  // user has connected their wallet
+
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🍪DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  };
+
+  const mintNft = () => {
+    setIsClaiming(true);
+    bundleDropModule
+    .claim("0", 1)
+    .then(() => {
+      setHasClaimedNFT(true);
+      console.log(
+        `🌊 Successfully Minted! Check it our on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address.toLowerCase()}/0`
+      );
+    })
+    .catch((err) => {
+      console.error("failed to claim", err);
+    })
+    .finally(() => {
+      setIsClaiming(false);
+    });
+  }
+
   return (
-    <div className="landing">
-      <h1>👀 wallet connected, now what!</h1>
-    </div>);
+    <div className="mint-nft">
+      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <button
+        disabled={isClaiming}
+        onClick={() => mintNft()}
+      >
+        {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+      </button>
+    </div>
+  );
 };
 
 export default App;
