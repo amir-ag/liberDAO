eec1f694f669ccf25ca02e67327e94d482735e71
Amir <40580705+amir-ag@users.noreply.github.com>
2022-02-06 17:49:22 +0100

    revoke admin rights

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,25 @@
+import sdk from "./1-initialize-sdk.js";
+
+const tokenModule = sdk.getTokenModule(
+  "0x5b3E74B1104BB67a405759682A3613426A122823",
+);
+
+(async () => {
+  try {
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
